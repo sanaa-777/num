44e10c614a9d03cab1b7113a9cd569f1613142a8
Yemen Directory Dev <dev@yemendirectory.net>
2026-08-10 03:52:26 +0800

    fix: add cache-busting version to build-meta.js script tag
    - Prevents browser from using cached build-meta.js with old version number
    - Ensures version-manager detects version changes and clears cache

--- app/build-meta.js
+++ app/build-meta.js
@@ -1,5 +1,5 @@
 window.__APP_BUILD__ = Object.freeze({
-  version: '20260809194654',
-  commit: '2da07e5',
-  deployedAt: '2026-08-09T19:46:54.600Z'
+  version: '20260809195216',
+  commit: 'f2e9339',
+  deployedAt: '2026-08-09T19:52:16.835Z'
 });
